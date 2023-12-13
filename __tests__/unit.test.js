e738df0bb5f24a0ac559ccce142b1b9eb8e7e274
Murphy <thehelixpg@gmail.com>
2023-12-13 11:42:42 +0200

    fix import statements

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,3 +1,1 @@
 import { AbstractUnit, Angular, Distance, Velocity, Weight, Temperature, Pressure, Energy, Unit, UnitPropsDict } from "../src/units.js";
-
-export { AbstractUnit, Angular, Distance, Velocity, Weight, Temperature, Pressure, Energy, Unit, UnitPropsDict }
